3ddefd550258b5ad431a4b383de7d09f3d231ac5
Rasmus Meinecke <rasmusfabius83@gmai.com>
2019-10-29 21:22:12 +0100

    javascript added

--- M2-D5.js
+++ M2-D5.js
@@ -23,8 +23,8 @@ function addLink(name) {
 /*   EX12) Write a function to remove the Search Looking class item */
 
 function removeItem() {
-  var element = document.querySelector(".text-muted");
-  element.remove();
+  var search = document.querySelector("header > div > div:last-child a:first-child");
+  search.remove();
 }
 
 // EX13) Write a function to change the background of the jumbotron
@@ -38,28 +38,65 @@ function changeBackground(color) {
 //    EX14) Write a function to change the color of the main title
 
 function changeColorMainTitle(color) {
-  var mainTitle = document.querySelector("h1");
-  mainTitle.style.color = color;
+  document.querySelector("h1").style.color = color;
 }
 
 //  EX15) Write a function to change the column size for post headings
 
+function changeColumnSize(size) {
+  var posts = document.querySelectorAll(".blog-post");
+  posts.forEach(function(post) {
+    post.style.width = size + "px";
+  });
+}
+
 //EX16) Write a function to remove all the links under "Elsewhere"
 
 function removeElsewhere() {
-  var ol = document.querySelector(".list-unstyled");
-  var listLength = ol.children.length;
-
-  for (i = 0; i < listLength; i++) {
-    ol.removeChild(ol.children[0]);
+  var toBeRemoved = document.querySelector("aside div:last-child ol");
+  toBeRemoved.remove();
+}
+//  EX17) Write a function to trim just the first 150 characters for each blog post
+function trim100() {
+  var firstPostParagraph = document.querySelectorAll(".blog-post .blog-post-meta + p");
+  // firstPostParagraph[0].innerText = firstPostParagraph[0].innerText.substring(0, 100);
+  for (var i = 0; i < firstPostParagraph.length; i++) {
+    firstPostParagraph[i].innerText = firstPostParagraph[i].innerText.substring(0, 100);
   }
 }
-
-//  EX17) Write a function to trim just the first 150 characters for each blog post
-function trim50() {
-  var blogPost = document.querySelectorAll(".blog-post").textContent;
-  console.log(blogPost);
-}
 //EX18) Write a function and attach it to the "Newer" button, to add new Blog Post (just div and title)
+function addBlogPost(blogName) {
+  var newblogPost = document.createElement("div");
+  newblogPost.className = "blog-post";
+  var title = document.createElement("h2");
+  title.textContent = blogName;
+  newblogPost.append(title);
+  var paginationBlog = document.querySelector(".blog-pagination");
+  paginationBlog.before(newblogPost);
+}
+var buttonNewer = document.querySelector(".blog-pagination a:last-child");
+buttonNewer.onclick = function(event) {
+  event.preventDefault();
+  addBlogPost("rasmus");
+};
+
 //EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post
+
+var buttonOlder = document.querySelector(".blog-pagination a:first-child");
+
+function removeBlogPost() {
+  var tbr = document.querySelector(".blog-post:last-of-type");
+  tbr.remove();
+}
+
 //EX20) Write an alert with the name of the author every time the user hover with the mouse over an author name
+
+var authors = document.querySelectorAll(".blog-post-meta a");
+
+function onHover(event) {
+  console.log("name is", event.target.textContent);
+}
+
+for (var authorElements of authors) {
+  authorElements.onmouseover = onHover;
+}
